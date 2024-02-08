53d6a3a6c89bdd4b6613fbb4816eb4cb00b0f913
sm9940 <elation4927@gmail.com>
2024-02-08 16:20:52 +0900

    240208

--- src/ch03/Quiz9.java
+++ src/ch03/Quiz9.java
@@ -1,11 +1,21 @@
 package ch03;
 
+import java.util.Scanner;
+
 public class Quiz9 {
     public static void main(String[] args) {
-        int num = -10 ;
-        String result =(num<0)?"음수":(num>0)?"양수":"0";
+//        int num = -10 ;
+//        String result =(num<0)?"음수":(num>0)?"양수":"0";
+//
+//        System.out.println(result);
 
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("입력: ");
+        int num = scanner.nextInt();
+
+        //조건 3개 이상-> 삼항연산자 중첩
+
+        System.out.println(num>0?"양수":(num<0 ? "음수":"0"));
     }
 }
