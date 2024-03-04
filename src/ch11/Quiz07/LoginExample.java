d033b02bc475677994786d94831452395938f3f4
sm9940 <elation4927@gmail.com>
2024-03-04 14:15:04 +0900

    240304

---
+++ src/ch11/Quiz07/LoginExample.java
@@ -0,0 +1,25 @@
+package ch11.Quiz07;
+
+public class LoginExample {
+    public static void main(String[] args) {
+        try {
+            login("white", "12345");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            login("blue", "54321");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+public static void login(String id,String password) throws Exception {
+        if(!id.equals("blue")){
+           throw  new NotExistIDException("아이디가 존재하지않습니다.");
+        }
+        if (!password.equals("12345")){
+           throw  new WrongPasswordException("패스워드가 틀립니다.");
+        }
+}
+}
