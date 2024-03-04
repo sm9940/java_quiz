453dfa7a2875b17a3ccf3778ff88e65a48ce73b3
sm9940 <elation4927@gmail.com>
2024-03-05 01:17:30 +0900

    240305

--- src/ch04/Quiz7.java
+++ src/ch04/Quiz7.java
@@ -6,7 +6,7 @@ public class Quiz7 {
         int sum2 = 1;
         int sum3 = 0;
         System.out.print(sum1+","+sum2);
-        for(int x = 0 ; x<8; x++){
+        for(int x = 3 ; x<=10; x++){
             sum3 = sum1 + sum2;
             System.out.print("," + sum3);
             sum1 = sum2;
