b7452232df7abb283df45daa9995a040f2b8d136
sm9940 <elation4927@gmail.com>
2024-03-06 13:58:46 +0900

    240306

--- src/ch12/Quiz05/StringBuilderExample.java
+++ src/ch12/Quiz05/StringBuilderExample.java
@@ -4,16 +4,15 @@ public class StringBuilderExample {
     public static void main(String[] args) {
         String str = "";
 
+        StringBuilder sb = new StringBuilder();
 
 
         for (int i = 0; i <= 100 ; i++) {
-            str = new StringBuilder()
-                    .append(str+=i)
-                    .toString();
+           sb.append(i);
 
 
         }
-
+str = sb.toString();
         System.out.println(str);
     }
 }
